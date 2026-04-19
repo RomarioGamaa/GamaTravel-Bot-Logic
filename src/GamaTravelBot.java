aa1669b9a5646da2131475996f2ee15530c6036d
Romario Gamaa <gamaromario321@gmail.com>
2026-04-19 09:21:44 -0300

    Fix: Transformando o projeto em Maven

--- src/GamaTravelBot.java
+++ src/GamaTravelBot.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList; // Importante para a lista
 import java.util.Scanner;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class GamaTravelBot {
     public static void main(String[] args) {
